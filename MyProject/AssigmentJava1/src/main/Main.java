997b2320459d70685032b841664d14bc8a1c3587
sonnvth1807031 <sonnvth1807031@fpt.edu.vn>
2019-04-09 03:40:05 +0700

    first commit

---
+++ MyProject/AssigmentJava1/src/main/Main.java
@@ -0,0 +1,9 @@
+package main;
+import controller.createMenu;
+
+public class Main {
+
+    public static void main(String[] args) {
+        createMenu createMenu = new createMenu();
+        createMenu.menu();
+    }}
